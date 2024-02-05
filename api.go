1831a9b2d6e314b679e1dbdbae5315979ae258cf
akakou <akakou571@gmail.com>
2024-02-05 19:49:05 +0900

    feat: add resp for` /certificates` api

--- api.go
+++ api.go
@@ -5,14 +5,14 @@ import (
 	"net/url"
 )
 
-func (api *MetaCTApi) Subscribe(domain string) (*OnlyStatusResp, error) {
+func (api *MetaCTApi) Subscribe(domain string) (*SubscribeResp, error) {
 	u := fmt.Sprintf(SUBSCRIBED_DOMAINS_API, api.AppId)
 
 	data := url.Values{}
 	data.Set("subscribe", domain)
 	data.Set("access_token", api.AccessToken)
 
-	result, err := fetchAPI[OnlyStatusResp]("POST", u, data)
+	result, err := fetchAPI[SubscribeResp]("POST", u, data)
 
 	if err != nil {
 		return nil, fmt.Errorf("error subscribing: %v", err)
@@ -25,14 +25,14 @@ func (api *MetaCTApi) Subscribe(domain string) (*OnlyStatusResp, error) {
 	return result, nil
 }
 
-func (api *MetaCTApi) Unsubscribe(domain string) (*OnlyStatusResp, error) {
+func (api *MetaCTApi) Unsubscribe(domain string) (*SubscribeResp, error) {
 	u := fmt.Sprintf(SUBSCRIBED_DOMAINS_API, api.AppId)
 
 	data := url.Values{}
 	data.Set("unsubscribe", domain)
 	data.Set("access_token", api.AccessToken)
 
-	result, err := fetchAPI[OnlyStatusResp]("POST", u, data)
+	result, err := fetchAPI[SubscribeResp]("POST", u, data)
 
 	if err != nil {
 		return nil, fmt.Errorf("error unsubscribing: %v", err)
@@ -45,14 +45,14 @@ func (api *MetaCTApi) Unsubscribe(domain string) (*OnlyStatusResp, error) {
 	return result, nil
 }
 
-func (api *MetaCTApi) SubscribeList() (*SubscribeList, error) {
+func (api *MetaCTApi) SubscribeList() (*SubscribeListResp, error) {
 	u := fmt.Sprintf(SUBSCRIBED_DOMAINS_API, api.AppId)
 
 	data := url.Values{}
 	data.Set("fields", "domain")
 	data.Set("access_token", api.AccessToken)
 
-	result, err := fetchAPI[SubscribeList]("GET", u, data)
+	result, err := fetchAPI[SubscribeListResp]("GET", u, data)
 
 	if err != nil {
 		return nil, fmt.Errorf("error getting certificates: %v", err)
--- resp.go
+++ resp.go
@@ -1,18 +1,51 @@
 package metactapi
 
-type SubscribeList struct {
+type SubscribeResp struct {
+	Success bool `json:"success"`
+}
+
+type Paging struct {
+	Cursors struct {
+		Before string `json:"before"`
+		After  string `json:"after"`
+	} `json:"causers"`
+}
+
+type SubscribeListResp struct {
 	Data []struct {
 		Id     string `json:"id"`
 		Domain string `json:"domain"`
 	} `json:"data"`
-	Paging struct {
-		Cursors struct {
-			Before string `json:"before"`
-			After  string `json:"after"`
-		} `json:"cursors"`
-	} `json:"paging"`
+	Paging `json:"paging"`
 }
 
-type OnlyStatusResp struct {
-	Success bool `json:"success"`
+type CertificatesResp struct {
+	Data []struct {
+		Id                     string `json:"id"`
+		AuthorityKeyIdentifier string `json:"authority_key_identifier"`
+		BasicConstraints       string `json:"basic_constraints"`
+		CertHashMd5            string `json:"cert_hash_md5"`
+		CertHashSha1           string `json:"cert_hash_sha1"`
+		CertHashSha256         string `json:"cert_hash_sha256"`
+		CertificatePem         string `json:"certificate_pem"`
+		Domains                string `json:"domains"`
+		ExtendedKeyUsage       string `json:"extended_key_usage"`
+		Extensions             string `json:"extensions"`
+		IssuerName             string `json:"issuer_name"`
+		KeyUsage               string `json:"key_usage"`
+		NotValidAfter          string `json:"not_valid_after"`
+		NotValidBefore         string `json:"not_valid_before"`
+		PublicKeyAlgorithm     string `json:"public_key_algorithm"`
+		PublicKeyHashSha256    string `json:"public_key_hash_sha256"`
+		PublicKeyPem           string `json:"public_key_pem"`
+		PublicKeySize          string `json:"public_key_size"`
+		PublicKeyValues        string `json:"public_key_values"`
+		SerialNumber           string `json:"serial_number"`
+		SignatureAlgorithm     string `json:"signature_algorithm"`
+		SignatureValue         string `json:"signature_value"`
+		SubjectKeyIdentifier   string `json:"subject_key_identifier"`
+		SubjectName            string `json:"subject_name"`
+		Version                string `json:"version"`
+	} `json:"data"`
+	Paging `json:"paging"`
 }
