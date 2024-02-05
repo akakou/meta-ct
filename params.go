f9c47dd2ee59d753a39995fc1e917ff4a74fd613
akakou <akakou571@gmail.com>
2024-02-05 19:15:25 +0900

    feat: endpoint configuration

---
+++ params.go
@@ -0,0 +1,8 @@
+package metactapi
+
+const API_BASE = "https://graph.facebook.com"
+
+const (
+	SUBSCRIBED_DOMAINS_API = API_BASE + "/%v/subscribed_domains"
+	CERTFICATES_API        = API_BASE + "/certificates"
+)
