4abbc15f9d893457725ad43f10147906537971bc
akakou <akakou571@gmail.com>
2024-02-05 20:49:29 +0900

    feat: parse webhook certificates

---
+++ webhook.go
@@ -0,0 +1,36 @@
+package metactapi
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Notification struct {
+	Entry []struct {
+		ID            string   `json:"id"`
+		ChangedFields []string `json:"changed_fields"`
+		Time          int      `json:"time"`
+		Certificate   `json:"certificate"`
+	} `json:"entry"`
+	Object string `json:"object"`
+}
+
+func (api *MetaCTApi) WebHookCertificates(c echo.Context) ([]Certificate, error) {
+	var notification Notification
+	if err := c.Bind(&notification); err != nil {
+		return nil, fmt.Errorf("error parsing payload: %v", err)
+	}
+
+	if notification.Object != "subscribed_domain" {
+		return nil, fmt.Errorf("invalid object: %s", notification.Object)
+	}
+
+	certs := []Certificate{}
+
+	for _, entry := range notification.Entry {
+		certs = append(certs, entry.Certificate)
+	}
+
+	return certs, nil
+}
