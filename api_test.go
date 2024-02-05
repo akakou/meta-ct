7b9aca9b75df70698f8056beff3a4f5858baadda
akakou <akakou571@gmail.com>
2024-02-05 20:13:35 +0900

    test: get certficates

--- api_test.go
+++ api_test.go
@@ -6,32 +6,75 @@ import (
 	"testing"
 )
 
-func TestActualSubscribe(t *testing.T) {
+var debugPrint = false
+
+func debugPrintf(tag string, data any) {
+	if debugPrint {
+		fmt.Printf("%v: %v\n", tag, data)
+	}
+}
+
+const example_domain = "hoge.ochano.co"
+
+func TestActual(t *testing.T) {
 	appId := os.Getenv("META_APP_ID")
 	accessToken := os.Getenv("META_ACCESS_TOKEN")
 
 	api := NewCT(appId, accessToken)
 
-	resp, err := api.Subscribe("example.com")
+	resp, err := api.Subscribe(example_domain)
 
 	if err != nil {
-		t.Fatalf("error subscribing: %v", err)
+		t.Fatalf("error subscribe: %v", err)
 	}
 
-	fmt.Printf("%v", resp)
+	debugPrintf("subscribe", resp)
 
 	list, err := api.SubscribeList()
 	if err != nil {
-		t.Fatalf("error subscribing: %v", err)
+		t.Fatalf("error subscribe list: %v", err)
 	}
 
-	fmt.Printf("%v", list)
+	debugPrintf("list", list)
 
-	resp, err = api.Unsubscribe("example.com")
+	certs, err := api.Certificates(example_domain, []string{
+		"authority_key_identifier",
+		"basic_constraints",
+		"cert_hash_md5",
+		"cert_hash_sha1",
+		"cert_hash_sha256",
+		"certificate_pem",
+		"domains",
+		"extended_key_usage",
+		"extensions",
+		"issuer_name",
+		"key_usage",
+		"not_valid_after",
+		"not_valid_before",
+		"public_key_algorithm",
+		"public_key_hash_sha256",
+		"public_key_pem",
+		"public_key_size",
+		"public_key_values",
+		"serial_number",
+		"signature_algorithm",
+		"signature_value",
+		"subject_key_identifier",
+		"subject_name",
+		"version",
+	})
 
 	if err != nil {
-		t.Fatalf("error subscribing: %v", err)
+		t.Fatalf("error certs: %v", err)
 	}
 
-	fmt.Printf("%v", resp)
+	debugPrintf("certs", certs)
+
+	resp, err = api.Unsubscribe(example_domain)
+
+	if err != nil {
+		t.Fatalf("error unsubscribe: %v", err)
+	}
+
+	debugPrintf("resp", resp)
 }
