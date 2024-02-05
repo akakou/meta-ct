324dc9e3f9ce8a55648745b0907ee60410a23d21
akakou <akakou571@gmail.com>
2024-02-05 19:19:15 +0900

    feat: un-subscribe

--- subscribe.go
+++ subscribe.go
@@ -26,5 +26,25 @@ func (api *MetaCTApi) Subscribe(domain string) (*OnlyStatusResp, error) {
 		return nil, fmt.Errorf("error subscribing: %v", result)
 	}
 
-	return result, err
+	return result, nil
+}
+
+func (api *MetaCTApi) Unsubscribe(domain string) (*OnlyStatusResp, error) {
+	u := fmt.Sprintf(SUBSCRIBED_DOMAINS_API, api.AppId)
+
+	data := url.Values{}
+	data.Set("unsubscribe", domain)
+	data.Set("access_token", api.AccessToken)
+
+	result, err := fetchAPI[OnlyStatusResp]("POST", u, data)
+
+	if err != nil {
+		return nil, fmt.Errorf("error unsubscribing: %v", err)
+	}
+
+	if !result.Success {
+		return nil, fmt.Errorf("error unsubscribing: %v", result)
+	}
+
+	return result, nil
 }
