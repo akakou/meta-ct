3e2aa564e48b8a4e647c79a46b3a3dbc81863221
akakou <akakou571@gmail.com>
2024-02-05 19:37:19 +0900

    feat: fetch subscribe list

--- subscribe.go
+++ subscribe.go
@@ -5,6 +5,19 @@ import (
 	"net/url"
 )
 
+type SubscribeList struct {
+	Data []struct {
+		Id     string `json:"id"`
+		Domain string `json:"domain"`
+	} `json:"data"`
+	Paging struct {
+		Cursors struct {
+			Before string `json:"before"`
+			After  string `json:"after"`
+		} `json:"cursors"`
+	} `json:"paging"`
+}
+
 type OnlyStatusResp struct {
 	Success bool `json:"success"`
 }
@@ -44,6 +57,22 @@ func (api *MetaCTApi) Unsubscribe(domain string) (*OnlyStatusResp, error) {
 
 	if !result.Success {
 		return nil, fmt.Errorf("error unsubscribing: %v", result)
+	}
+
+	return result, nil
+}
+
+func (api *MetaCTApi) SubscribeList() (*SubscribeList, error) {
+	u := fmt.Sprintf(SUBSCRIBED_DOMAINS_API, api.AppId)
+
+	data := url.Values{}
+	data.Set("fields", "domain")
+	data.Set("access_token", api.AccessToken)
+
+	result, err := fetchAPI[SubscribeList]("GET", u, data)
+
+	if err != nil {
+		return nil, fmt.Errorf("error getting certificates: %v", err)
 	}
 
 	return result, nil
