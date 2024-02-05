1129555b9001d7e31bd26c62716783ccabb21b1e
akakou <akakou571@gmail.com>
2024-02-05 17:39:56 +0900

    feat: init object

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package metactapi
+
+type MetaCTApi struct {
+	AppId       int
+	AccessToken string
+}
+
+func NewCT(appId int, credentials string) *MetaCTApi {
+	return &MetaCTApi{
+		AppId:       appId,
+		AccessToken: credentials,
+	}
+}
