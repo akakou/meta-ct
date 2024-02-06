4c5ed459579710648de6bd5ee461b633838e8677
akakou <akakou571@gmail.com>
2024-02-07 04:08:36 +0900

    feat: example webhook

--- example/main.go
+++ example/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"os"
+
+	metact "github.com/akakou/meta-ct"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	appId := os.Getenv("META_APP_ID")
+	accessToken := os.Getenv("META_ACCESS_TOKEN")
+	testDomain := os.Getenv("TEST_DOMAIN")
+
+	ct := metact.NewCT(appId, accessToken)
+	err := ct.Subscribe(testDomain)
+
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
 	e.GET("/", func(c echo.Context) error {
+		certs, err := ct.WebHookCertificates(c)
+		if err != nil {
+			c.Error(err)
+		}
+
+		fmt.Printf("certs: %v", certs)
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	err = e.Start(":1323")
+	e.Logger.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -18,3 +18,5 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/akakou/meta-ct => ../
